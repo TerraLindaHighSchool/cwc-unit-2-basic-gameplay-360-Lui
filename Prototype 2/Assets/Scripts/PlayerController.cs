e58410f6b57c0b2069fc8c1fa003a9aa93143486
360-Lui <360-Lui@users.noreply.github.com>
2020-01-19 14:48:34 -0800

    Lesson 2.2.4 completed

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -30,5 +30,10 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch a projectile from the player 
+        }
     }
 }
